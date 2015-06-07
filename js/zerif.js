0b6dbd194778aa6ded5afb63678496ec4fabd158
Minako Aikawa <mi_ko64@hotmail.com>
2015-06-07 20:46:34 +0900

    サンプル写真はなくしました！

--- js/zerif.js
+++ js/zerif.js
@@ -4,9 +4,10 @@
 $.vegas('slideshow', {
   delay:7000,
   backgrounds:[
-    { src:'images/backgrounds/bg1.jpg', fade:1000 },
     { src:'images/backgrounds/bg2.jpg', fade:1000 },
-    { src:'images/backgrounds/bg3.jpg', fade:1000 }
+    { src:'images/backgrounds/bg3.jpg', fade:1000 },
+    { src:'images/backgrounds/bg4.jpg', fade:1000 },
+    { src:'images/backgrounds/bg.jpg', fade:1000 },
   ]
 });
       
