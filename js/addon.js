2168b4b5db81233585e4105dfcd0416ac23b4534
hase62 <tk.hasegawa@gmail.com>
2018-01-07 17:37:39 +0900

    Minor Update

--- js/addon.js
+++
@@ -1,9 +0,0 @@
-num = 2; // 入れ替える画像の枚数(最初の画像も含める)
-nme = "img/miup_pack1” // 画像のディレクトリとファイル名の数字と拡張子より前の部分
-exp = "jpg" // 拡張子
-cnt = 0;
-function changeImage() {
-  cnt++;
-  cnt %= num;
-  document.img.src = nme + cnt + "." + exp;
-}
